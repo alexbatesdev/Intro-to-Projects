552c40c5afadaf8be27febd4f5c4626473e4126d
Alex <mcbuzzer@gmail.com>
2022-08-24 15:24:29 -0600

    Game Over functionality

--- app.js
+++ app.js
@@ -90,6 +90,19 @@ class SceneStore extends Phaser.Scene {
 
 };
 
+class SceneGameOver extends Phaser.Scene {
+    constructor() {
+        super({ key: 'gameOver'});
+    }
+    preload(){
+        
+        
+    }    
+    create() {
+        this.add.text(20, 20, "Game Over", { font: "24px Arial", fill: "#ffffff" });
+    } 
+
+};
 
 class SceneGame extends Phaser.Scene {
     constructor() {
@@ -174,6 +187,10 @@ class SceneGame extends Phaser.Scene {
         this.towers.fireAll(time, delta);
         this.plrHealthText.setText(this.player.money);
         this.plrMoneyText.setText(this.player.health);
+
+        if (this.player.health <= 0) {
+            this.scene.start('gameOver');
+        }
     }
 };
 
